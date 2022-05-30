08ca4df0ab00517cef275603e5dbe52a0ff4c237
riti2009 <67743556+riti2009@users.noreply.github.com>
2022-05-30 15:16:24 +0530

    Add files via upload
    Class -10, checking the average of weights

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var weight = [13, 22, 15, 7];
+var sum = weight[0] + weight[1] + weight[2] + weight[3] ;
+var av = sum/weight.length;
+console.log(av);
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
